20b0d7ddaf0cf9de9777270c865d4d2e46e71f72
salvoPernicano <salvo.pernicano@gmail.com>
2024-01-08 17:03:17 +0100

    feat nome passeggero/ nome tariffa

--- main.js
+++ main.js
@@ -2,12 +2,18 @@ function confirm() {
     let kilometri = document.getElementById("kilometri").value;
     let age = document.getElementById("age").value;
     let tariffa = kilometri * 0.21;
+    let nomePasseggero = document.getElementById("name").value;
   
+document.getElementById("nome").innerHTML = nomePasseggero;
+
     if (age === "minor") {
-      document.getElementById("price").innerHTML = `Il suo prezzo è: €${((tariffa * 80) / 100).toFixed(2)}`;
+      document.getElementById("price").innerHTML = `${((tariffa * 80) / 100).toFixed(2)}€`;
+      document.getElementById("offer").innerHTML = "Biglietto ridotto minor"
   } else if (age === "senior") {
-    document.getElementById("price").innerHTML = `Il suo prezzo è: €${((tariffa * 60) / 100).toFixed(2)}`;
+    document.getElementById("price").innerHTML = `${((tariffa * 60) / 100).toFixed(2)}€`;
+    document.getElementById("offer").innerHTML = "Biglietto ridotto senior"
   } else if (age === "grown"){
-    document.getElementById("price").innerHTML = `Il suo prezzo è: €${tariffa.toFixed(2)}`;
+    document.getElementById("price").innerHTML = `${tariffa.toFixed(2)}€`;
+    document.getElementById("offer").innerHTML = "Biglietto standard"
   }
 }
