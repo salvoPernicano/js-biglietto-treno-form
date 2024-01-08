b65b7bb123f9891642ac7be0cbdf38d7b8611a14
salvoPernicano <salvo.pernicano@gmail.com>
2024-01-08 17:25:08 +0100

    fix stile/ feat comparsa biglietto

--- main.js
+++ main.js
@@ -5,10 +5,13 @@ function confirm() {
     let nomePasseggero = document.getElementById("name").value;
     let codice = Math.floor(Math.random()* 99999 + 1);
     let posto = Math.floor(Math.random()* 9 + 1);
+    const generazione = document.querySelector(".ticket");
+
   
 document.getElementById("nome").innerHTML = nomePasseggero;
 document.getElementById("codiceCp").innerHTML = codice;
 document.getElementById("carrozza").innerHTML = posto;
+generazione.classList.toggle("genera")
 
     if (age === "minor") {
       document.getElementById("price").innerHTML = `${((tariffa * 80) / 100).toFixed(2)}€`;
@@ -20,4 +23,9 @@ document.getElementById("carrozza").innerHTML = posto;
     document.getElementById("price").innerHTML = `${tariffa.toFixed(2)}€`;
     document.getElementById("offer").innerHTML = "Biglietto standard"
   }
+}
+
+function rimuovi(){
+    const rimuovi = document.querySelector(".ticket");
+    rimuovi.classList.toggle("genera");
 }
